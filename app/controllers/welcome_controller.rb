31fa19bc896335c1b965156a15dd9622d1e4ccd2
zhongdaoyou <18942371859@163.com>
2017-06-11 12:07:57 +0800

    the rest things

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = " Nice to meet you! "
+    flash[:alert] = " Nice to meet you! "
+    flash[:warning] = " Nice to meet you! "
+  end
 end
